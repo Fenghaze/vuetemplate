d635477cc4aa4d75d30feba0003530a94b7f64ec
Fenghaze <40448037+Fenghaze@users.noreply.github.com>
2020-05-02 20:30:58 +0800

    Create store.js

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+import axios from 'axios'
+Vue.use(Vuex)
+var store = new Vuex.Store({
+    state: {
+    },
+    mutations: {
+    }
+})
+export default store
